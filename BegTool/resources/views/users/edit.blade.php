bddf7ae43d4b97e8ca20e60d198c2d19cccc3f25
fabiano <fabian.brammer@outlook.com>
2015-07-29 15:47:38 +0200

    anmelden

--- BegTool/resources/views/users/edit.blade.php
+++ BegTool/resources/views/users/edit.blade.php
@@ -14,15 +14,15 @@
     {!! Form::text('birthdate', null, array('class'=>'form-control', 'placeholder'=>'Datum')) !!}          
         <span class="input-group-addon"><span class="glyphicon glyphicon-calendar"></span></span>
     </div>
-    {!! Form::label('email', 'E-Mail') !!}
+ <!--   {!! Form::label('email', 'E-Mail') !!}
     {!! Form::text('email', null, array('class'=>'form-control', 'placeholder'=>'E-Mail')) !!}
-    {!! Form::label('username', 'Benutzername') !!}
-    {!! Form::text('username', null, array('class'=>'form-control', 'placeholder'=>'Benutzername')) !!}
+     {!! Form::label('username', 'Benutzername') !!}
+     {!! Form::text('username', null, array('class'=>'form-control', 'placeholder'=>'Benutzername')) !!}
     {!! Form::label('password', 'Passwort') !!}
     {!! Form::password('password', array('class'=>'form-control', 'placeholder'=>'Passwort')) !!}
     {!! Form::label('password_confirmation', 'Passwort wiederholen') !!}
     {!! Form::password('password_confirmation', array('class'=>'form-control', 'placeholder'=>'Passwort wiederholen')) !!}
-    <br/>
+    <br/>-->
     {!! Form::submit('Ändern', array('class'=>'btn btn-large btn-primary btn-block'))!!}
 {!! Form::close() !!}
 <br/>
