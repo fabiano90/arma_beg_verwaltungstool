5db6180ea1e270b7b2a5d4b555f3441c276ff1a3
SagIch <SagIch@Lappi>
2015-07-30 11:40:30 +0200

    indexwiederda

---
+++ BegTool/resources/views/users/index.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.main')
+
+@section('content')
+<h2>Benutzer</h2>
+<div class="table-responsive">
+	<table class="table table-striped table-hover">
+		<thead>
+			<tr>
+				<th>Name</th>
+				<th>Vorname</th>
+				<th>Benutzername</th>
+				<th width="40%">Bearbeiten</th>
+			</tr>
+		</thead>
+		<tbody>
+			@foreach($users as $user)
+			<tr>
+				<td>{!! $user->lastname !!}</td>
+				<td>{!! $user->firstname !!}</td>
+				<td>{!! $user->username !!}</td>
+				<td>
+					<div class="btn-group">{!! HTML::link('/users/addfriend/'.$user->id, 'Freund hinzufÃ¼gen', array('class'=>'btn btn-default')) !!}
+					</div>
+				</td>
+			</tr>
+			@endforeach
+		</tbody>
+	</table>
+</div>
+{!! str_replace('/?', '?', $users->render()) !!}
+<br/>
+
+
+{!! HTML::link('users/register', 'Hinzufügen', array('class' => 'btn btn-default'))!!}
+{!! HTML::link('#', 'Zurück', array('class' => 'btn btn-default', 'onClick="javascript:history.back();return false;"'))!!}
+@stop
