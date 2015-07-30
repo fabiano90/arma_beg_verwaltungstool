c9b7dd92025fac5250f54535595e3483fdd87208
SagIch <SagIch@Lappi>
2015-07-30 13:06:10 +0200

    test

---
+++ BegTool/database/migrations/2015_07_30_095327_create_birthdays_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBirthdaysTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('birthdays', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->date('birthdate');//datetime??
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
--- BegTool/resources/views/layouts/main.blade.php
+++ BegTool/resources/views/layouts/main.blade.php
@@ -1,18 +1,70 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<title>ARMA</title>
-	<!-- Latest compiled and minified CSS -->
-	{!! HTML::style('css/bootstrap.min.css') !!}
-	{!! HTML::style('css/bootstrap-theme.min.css') !!}
-	{!! HTML::style('css/style.css') !!}
-	{!! HTML::script('js/jquery.min.js') !!}
-	{!! HTML::script('js/bootstrap.min.js') !!}
+<meta charset="UTF-8">
+<title>ARMA</title>
+<!-- Latest compiled and minified CSS -->
+{!! HTML::style('css/bootstrap.min.css') !!} {!!
+HTML::style('css/bootstrap-theme.min.css') !!} {!!
+HTML::style('css/style.css') !!} {!! HTML::script('js/jquery.min.js')
+!!} {!! HTML::script('js/bootstrap.min.js') !!}
 </head>
 <body>
-	<div class="container">
-		@yield('content')
+	<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Studentenverwaltung</title>
+<!-- Latest compiled and minified CSS -->
+{!! HTML::style('css/bootstrap.min.css') !!} {!!
+HTML::style('css/bootstrap-theme.min.css') !!} {!!
+HTML::style('css/bootstrap.datetimepicker.css') !!} {!!
+HTML::style('css/style.css') !!} {!! HTML::script('js/moment.js') !!}
+{!! HTML::script('js/jquery.min.js') !!} {!!
+HTML::script('js/bootstrap.min.js') !!} {!!
+HTML::script('js/bootstrap.datetimepicker.js') !!} {!!
+HTML::script('js/bootstrap.datetimepicker-de.js') !!}
+<script>
+		$(document).ready(function() {
+		      $('.datetimepicker').datetimepicker({
+		          language: 'de',
+		          pickTime: false,
+		          format: 'yyyy-mm-DD'
+		      });
+		  });
+	</script>
+</head>
+<body>
+	<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+		<div class="container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse"
+					data-target=".navbar-collapse">
+					<span class="sr-only">Navigation</span> <span class="icon-bar"></span>
+					<span class="icon-bar"></span> <span class="icon-bar"></span>
+				</button>
+				{!! HTML::link('/', 'Studentenverwaltung',
+				array('class'=>'navbar-brand')) !!}
+			</div>
+			<div class="collapse navbar-collapse">
+				<ul class="nav navbar-nav">
+					<li class="dropdown"><a href="#" class="dropdown-toggle"
+						data-toggle="dropdown">Studenten <span class="caret"></span></a>
+						<ul class="dropdown-menu" role="menu">{!!
+							HTML::link('users/index', 'Liste') !!} {!!
+							HTML::link('users/new', 'Hinzufügen') !!}
+						</ul></li>
+					<li class="dropdown"><a href="#" class="dropdown-toggle"
+						data-toggle="dropdown">Semster <span class="caret"></span></a>
+						<ul class="dropdown-menu" role="menu">{!! HTML::link('semesters/posts',
+							'Liste') !!} {!! HTML::link('semesters/friends', 'Hinzufügen') !!}
+						</ul></li>
+				</ul>
+			</div>
+			<!--/.nav-collapse -->
+		</div>
 	</div>
+
+	<div class="container">@yield('content')</div>
 </body>
 </html>
