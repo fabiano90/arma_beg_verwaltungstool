c61e1fa656c20ede909504b65870ef2ba948f1e6
fabiano <fabian.brammer@outlook.com>
2015-08-01 16:24:50 +0200

    service

--- BegTool/app/Http/Controllers/SundayserviceController.php
+++ BegTool/app/Http/Controllers/SundayserviceController.php
@@ -12,48 +12,33 @@ class SundayserviceController extends Controller {
 		$sundayservices = Sundayservice::all ();
 		return view ( 'sundayservices.index' )->with ( 'sundayservices', $sundayservices );
 	}
-	public function getNewsunday(){
+	public function getNewsunday() {
+		$users = User::all ();
+		// queries the clients db table, orders by client_name and lists client_name and id
+		$client_optons = $users->sundayservice ()->orderBy ( 'username', 'asc' )->lists ( 'username', 'id' );
 		
-			
-		$users=User::all();
-			// queries the clients db table, orders by client_name and lists client_name and id
-			$client_optons = $users->users()->orderBy('username', 'asc')->lists('username','id');
-		
-			return View::make('sundayservices.newSunday', array('client_options' => $client_options));
-		
-		
-		
+		return View::make ( 'sundayservices.newSunday', array (
+				'client_options' => $client_options 
+		) );
 	}
-	
-	
-	
-	
-	
-	
 	public function getNewyear($year) {
-
-		$sundays = $this->sundaysYear($year);
-		$users = User::all();
-		$sundayservices = Sundayservice::all();
-		return view ( 'sundayservices.newYear' )->with ( 'sundays', $sundays )->with('users', $users);
-	
+		$sundays = $this->sundaysYear ( $year );
+		$users = User::all ();
+		$sundayservices = Sundayservice::all ();
+		return view ( 'sundayservices.newYear' )->with ( 'sundays', $sundays )->with ( 'users', $users );
 	}
-	public function postNewyear(){
-
-		for($i=0;$i<52;$i++){
-		$sundayservices = new Sudayservice();
-		$sundayservices->user_id = Request::input('user_id'.$i);
-		$sundayservices->username = Request::input('username');
-		$sundayservices->email = Request::input('email');
-		$sundayservices->password = Hash::make(Request::input('password'));
-		$sundayservices->permission = 2;//intval(Request::input('permission'));
-	
-
-	
-		$user->save();
+	public function postNewyear() {
+		for($i = 0; $i < 52; $i ++) {
+			$sundayservices = new Sudayservice ();
+			$sundayservices->user_id = Request::input ( 'user_id' . $i );
+			$sundayservices->username = Request::input ( 'username' );
+			$sundayservices->email = Request::input ( 'email' );
+			$sundayservices->password = Hash::make ( Request::input ( 'password' ) );
+			$sundayservices->permission = 2; // intval(Request::input('permission'));
+			
+			$user->save ();
 		}
-		return redirect('users')->with('message', 'success|Jahr erfolgreich angelegt!');
-
+		return redirect ( 'users' )->with ( 'message', 'success|Jahr erfolgreich angelegt!' );
 	}
 	public function sundaysYear($year) {
 		$startDate = strtotime ( $year . '-01-01 23:59:59' );
@@ -62,7 +47,7 @@ class SundayserviceController extends Controller {
 		$nextYear = $year + 1;
 		
 		for($i = 0; strcmp ( date ( 'd.m.Y', $startDate ), '01.01.' . $nextYear ) != 0;) {
-				
+			
 			if (strcmp ( date ( 'D', $startDate ), 'Sun' ) == 0) {
 				$sundays [$i] = $startDate;
 				$i ++;
