8acf48189ee15dbe9c7abfc398368cb103fd909a
fabiano <fabian.brammer@outlook.com>
2015-09-03 13:27:26 +0200

    Startseite

---
+++ BegTool/resources/views/layouts/main.blade.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Beg Osnabr&uuml;ck</title>
+<!-- Latest compiled and minified CSS -->
+{!!HTML::style('css/footable/footable.standalone.css')!!}
+{!!HTML::style('css/footable/footable.core.css')!!}
+{!!HTML::style('css/bootstrap.min.css')!!}
+{!!HTML::style('css/bootstrap-theme.min.css')!!}
+{!!HTML::style('css/bootstrap.datetimepicker.css')!!}
+{!!HTML::style('css/offcanvas.css')!!}
+{!!HTML::style('css/style.css')!!}
+{!!HTML::style('css/stickysort-css/normalize.css')!!}
+{!!HTML::style('css/stickysort-css/stickysort.css')!!}
+{!!HTML::style('css/stickysort-css/styles.css')!!}
+
+
+
+<!-- JavaScript -->
+
+
+{!!HTML::script('js/moment.js')!!}
+{!!HTML::script('js/jquery.min.js')!!}
+{!!HTML::script('js/jquery.js')!!}
+{!!HTML::script('js/bootstrap.min.js')!!}
+{!!HTML::script('js/bootstrap.datetimepicker.js')!!}
+{!!HTML::script('js/bootstrap.datetimepicker-de.js')!!}
+{!!HTML::script('js/jquery.stickysort.js')!!}
+{!!HTML::script('js/offcanvas.js')!!}
+{!!HTML::script('js/jquery.ba-throttle-debounce.min.js')!!}
+{!!HTML::script('js/ScrollToFixed-master/jquery-scrolltofixed.js')!!}
+
+{!!HTML::script('js/footable-plugin/footable.js')!!}
+{!!HTML::script('js/footable-plugin/footable.plugin.template.js')!!}
+{!!HTML::script('js/footable-plugin/footable.sort.js')!!}
+{!!HTML::script('js/footable-plugin/footable.filter.js')!!}
+{!!HTML::script('js/footable-plugin/footable.paginate.js')!!}
+{!!HTML::script('js/useful.js')!!}
+
+
+
+</head>
+<body>
+
+
+	<div class="container">
+	<nav class="navbar navbar-default header">
+		<div class="container">
+
+			<div class="navbar-header">
+				<a id"title-menu" class="navbar-brand" href="/users">Logo</a>
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+					<span class="sr-only">Navigation ein-/ausblenden</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span> 
+					<span class="icon-bar"></span>
+				</button>
+				
+			</div>
+			<div id="navbar" class="collapse navbar-collapse">
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="/users"  title="Schreibe und empfange Nachrichten"><span class="glyphicon glyphicon-home" aria-hidden="true"></span></a></li>
+					<li><a href="/messages/chat/0#end"  title="Schreibe und empfange Nachrichten"><span class="glyphicon glyphicon-envelope" aria-hidden="true"></span>@yield('messages')</a></li>
+					<li><a href="/sundays" title="Hier kannst du deine Dienste einsehen und tauschen"><span class="glyphicon glyphicon-calendar" aria-hidden="true"></span></a></li>
+					<li><a href="/songs" title="Informationen zu Liedern"><span class="glyphicon glyphicon-music" aria-hidden="true"></span></a></li>
+					<li><a href="/members" title="Geburtstage, Emailadressen und Namen von Gemeindemitgliedern, Mitarbeitern und Freunden"><span class="glyphicon glyphicon-user" aria-hidden="true"></span></a></li>
+	            	<li><a href="/login/logout" title="Auslogen"><span class="glyphicon glyphicon-off" aria-hidden="true"></span></a></li>
+	            	<li class="dropdown" title="Hier geht es zu den Listen Kigo, Leitung und Predigten. Trage deine Dienste ein.">
+				          <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-pencil" aria-hidden="true"> <span class="caret"></span></a>
+				          <ul class="dropdown-menu" role="menu">
+				            {!! cleverLink('/kigos', 'Kigo') !!}
+	            			{!! cleverLink('/sundayservices', 'Leitung') !!} 
+	            			{!! cleverLink('/sermons', 'Predigten') !!}
+				            {!! cleverLink('/members', 'Nutzer') !!}  
+				          </ul>
+				    </li>
+	            	
+
+	            	
+				</ul>
+			</div>
+			<!-- /.nav-collapse -->
+		</div>
+		<!-- /.container -->
+	</nav>
+	<!-- /.navbar -->
+	{!! showMessageAndErrors(Session::get('message'), $errors->all()) !!}
+	@yield('menu')
+	
+	<div class="container">
+		@yield('mainslider')
+	</div>
+	<div class="container">
+		@yield('content')
+<div class="scroll-back-to-top-wrapper">
+<span class="scroll-back-to-top-inner">
+<i class="fa fa-lg fa-arrow-up"></i>
+</span>
+</div>
+	</div>
+	
+	
+
+
+				
+
+
+
+
+			<footer class"footer">
+				<p>&copy; Bekennende Evangelische Gemeinde Osnabr&uuml;ck </p>
+			</footer>
+
+		</div>
+
+
+	</body>
+</html>
--- BegTool/resources/views/users/index.blade.php
+++ BegTool/resources/views/users/index.blade.php
@@ -1,11 +1,5 @@
-@extends('layouts.main') 
-@section('messages')
-    @if($newMessages>'0')
-        <span class="label label-danger message-cound">{!!$newMessages." neu"!!}</span>
-    @endif 
-@stop
-
-@section('mainslider')
+@extends('layouts.main') @section('messages') @if($newMessages>'0')
+<span class="label label-danger message-cound">{!!$newMessages." neu"!!}</span> @endif @stop @section('mainslider')
 <div id="carousel-example-generic" class="carousel slide content-shadow content-box" data-ride="carousel">
     <!-- Positionsanzeiger -->
     <ol class="carousel-indicators">
@@ -15,46 +9,60 @@
     </ol>
 
     <!-- Verpackung für die Elemente -->
+
+
     <div class="carousel-inner" role="listbox">
-        <div class="item active">
-            Hallo {!!$user->username!!}, hier sind deine nächsten Dienste:
-            <div class="table-responsive">
-                <table data-page-size="5" class=" table-striped table-hover footable" data-filter="#filter">
-                     
-                    <thead>
-                        <tr>
-                            <th data-sort-initial="true">Datum</th>
-                            <th>Dienst</th>
-                            <th>Bearbeiten</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach($kigos as $kigo)
-                        <tr>
-                            <td data-type="numeric" data-value='{!! $kigo->date !!}'>{!! date('d.m.Y', $kigo->date) !!}</td>
-                            <td>Kigo</td>
-                            
-                        </tr>
-                        @endforeach 
-                        @foreach($lektors as $lektor)
-                        <tr>
-                            <td data-type="numeric" data-value='{!! $lektor->sermons->date !!}'>{!! date('d.m.Y', $lektor->sermons->date) !!}</td>
-                            <td>Leitung</td>
-                            
-                        </tr>
-                        @endforeach 
-                        @foreach($sermons as $sermon)
-                        <tr>
-                            <td data-type="numeric" data-value='{!! $sermon->date !!}'>{!! date('d.m.Y', $sermon->date) !!}</td>
-                            <td>Predigt</td>
-                            
-                        </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+            <div class="item active">
+            
+            <div class="row">
+                <div class="col-md-6">
+                    <h2>Hallo {!!$user->username!!}, willkommen zurück</h2>
+                    In den nächsten Wochen hast du folgende Dienste. <br/>
+                    Eine Komplette übesicht bekommst du im Kalender Menü.
+                </div>
+                <div class="col-md-6">
+                    <div class="table-responsive">
+                        <table data-page-size="5" class=" table-striped table-hover footable" data-filter="#filter">
+
+                            <thead class="hidden">
+                                <tr>
+                                    <th data-sort-initial="true">Datum</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($kigos as $kigo)
+                                <tr>
+                                    <td data-type="numeric" data-value='{!! $kigo->date !!}'>
+                                        <a class="pull-right" href="/kigos/editkigo/{!! $kigo->id !!}" class=""><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></a> {!! date('d.m.Y', $kigo->date) !!}
+                                        <h3>Kigo </h3>
+                                    </td>
+                                    
+                                </tr>
+                                @endforeach 
+                                @foreach($lektors as $lektor)
+                                <tr>
+                                    <td data-type="numeric" data-value='{!! $lektor->sermons->date !!}'>
+                                        <a class="pull-right"href="/sundayservices/editservice/{!! $lektor->id !!}"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></a> {!! date('d.m.Y', $lektor->sermons->date) !!}
+                                        <h3>Leitung </h3>
+                                    </td
+                                </tr>
+                                @endforeach 
+                                @foreach($sermons as $sermon)
+                                <tr>
+                                    <td data-type="numeric" data-value='{!! $sermon->date !!}'>
+                                        {!! date('d.m.Y', $sermon->date) !!} <a class="pull-right" href="/sermons/editsermon/{!! $sermon->preacher_id !!}"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></a> 
+                                        <h3>Predigt</h3>
+                                  </td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             </div>
 
         </div>
+
 
 
     </div>
@@ -69,28 +77,29 @@
         <span class="sr-only">Weiter</span>
     </a>
 </div>
-@stop
-@section('content')
+@stop @section('content')
 <div class="row">
     <div class="col-md-4">
         <a href="/messages/chat/0#end">
             <div class="content-shadow content-box text-center">
-                    <h2><span class="glyphicon glyphicon-envelope" aria-hidden="true"></span>@yield('messages') Nachrichten </h2>
+                <h2><span class="glyphicon glyphicon-envelope" aria-hidden="true"></span>@yield('messages') Nachrichten </h2>
             </div>
         </a>
     </div>
     <div class="col-md-4">
         <a href="/sundays">
             <div class="content-shadow content-box text-center">
-            <h2><span class="glyphicon glyphicon-calendar" aria-hidden="true"></span>  Übersicht</h2>
-        </div></a>
+                <h2><span class="glyphicon glyphicon-calendar" aria-hidden="true"></span>  Übersicht</h2>
+            </div>
+        </a>
     </div>
     <div class="col-md-4">
-      <a href="/songs">
-        <div class="content-shadow content-box text-center">
-                   <h2><span class="glyphicon glyphicon-music text-center" aria-hidden="true"></span>  Lieder</h2>
-           
-        </div></a>
+        <a href="/songs">
+            <div class="content-shadow content-box text-center">
+                <h2><span class="glyphicon glyphicon-music text-center" aria-hidden="true"></span>  Lieder</h2>
+
+            </div>
+        </a>
     </div>
 
 </div>
